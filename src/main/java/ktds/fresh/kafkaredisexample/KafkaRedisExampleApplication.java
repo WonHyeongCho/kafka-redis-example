8240e67d38ad3d888a8d46a54ec2c3145f895ed9
Wonhyeong Cho <whcho@kangwon.ac.kr>
2020-02-19 11:08:49 +0900

    1. Main 함수 KafkaReceiver 객체 추가 2020-02-19

--- src/main/java/ktds/fresh/kafkaredisexample/KafkaRedisExampleApplication.java
+++ src/main/java/ktds/fresh/kafkaredisexample/KafkaRedisExampleApplication.java
@@ -1,10 +1,15 @@
 package ktds.fresh.kafkaredisexample;
 
+import ktds.fresh.kafkaredisexample.kafka.Receiver.Receiver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KafkaRedisExampleApplication {
+
+    @Autowired
+    Receiver receiver;
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaRedisExampleApplication.class, args);
