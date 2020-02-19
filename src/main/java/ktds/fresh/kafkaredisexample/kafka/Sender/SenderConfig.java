1c1465579fafb6f706332ff4ac37be5d278702c8
Wonhyeong Cho <whcho@kangwon.ac.kr>
2020-02-19 10:58:23 +0900

    1. Kafka SenderConfig 수정 2020-02-19

---
+++ src/main/java/ktds/fresh/kafkaredisexample/kafka/Sender/SenderConfig.java
@@ -0,0 +1,45 @@
+package ktds.fresh.kafkaredisexample.kafka.Sender;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class SenderConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String brokers;
+
+    @Bean
+    public Map<String, Object> producerConfig(){
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return properties;
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public Sender sender(){
+        return new Sender();
+    }
+}
