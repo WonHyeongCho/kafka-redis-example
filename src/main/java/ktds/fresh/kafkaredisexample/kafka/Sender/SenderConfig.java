53c9a5680200928cd029aa8e177a27628d0039e3
Wonhyeong Cho <whcho@kangwon.ac.kr>
2020-02-19 12:58:42 +0900

    1. SenderConfig 설정 변경 2020-02-19

--- src/main/java/ktds/fresh/kafkaredisexample/kafka/Sender/SenderConfig.java
+++ src/main/java/ktds/fresh/kafkaredisexample/kafka/Sender/SenderConfig.java
@@ -1,7 +1,7 @@
 package ktds.fresh.kafkaredisexample.kafka.Sender;
 
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
